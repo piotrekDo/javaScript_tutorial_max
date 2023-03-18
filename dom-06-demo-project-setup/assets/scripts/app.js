e80130c1f30498e13f7701c526e7d9a89709dd5a
Piotrek Domagalski <piotr.domagalski@yahoo.com>
2023-03-18 16:51:00 +0100

    mini projekt formularz

--- dom-06-demo-project-setup/assets/scripts/app.js
+++ dom-06-demo-project-setup/assets/scripts/app.js
@@ -2,6 +2,8 @@ const addNewMovieBtn = document.getElementById("add-new-movie-btn");
 const backdrop = document.getElementById("backdrop");
 const addNewMovieModal = document.getElementById("add-modal");
 const cancelAddMovieBatton = addNewMovieModal.querySelector(".btn--passive");
+const confirmAddMovieButton = cancelAddMovieBatton.nextElementSibling;
+const inputElements = addNewMovieModal.querySelectorAll("input");
 
 const toggleBackdrop = () => {
   backdrop.classList.toggle("visible");
@@ -24,6 +26,22 @@ function cancelAddMovieHandler() {
   addNewMovieBtnClickHandler();
 }
 
+function confirmAddMovieButtonHandler() {
+  const titleValue = inputElements[0].value;
+  const imageUrlValue = inputElements[1].value;
+  const ratingValue = inputElements[2].value;
+  if (
+    titleValue.trim() === "" ||
+    imageUrlValue === "" ||
+    ratingValue === "" ||
+    +ratingValue < 1 ||
+    +ratingValue > 5
+  ) {
+    alert('Please enter a valid input');
+  }
+}
+
 addNewMovieBtn.addEventListener("click", addNewMovieBtnClickHandler);
 backdrop.addEventListener("click", backdropClickHandler);
 cancelAddMovieBatton.addEventListener("click", cancelAddMovieHandler);
+confirmAddMovieButton.addEventListener("click", confirmAddMovieButtonHandler);
