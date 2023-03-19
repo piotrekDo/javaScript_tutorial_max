a500d4718312f034aa96ba35fed189cf3e278c32
Piotrek Domagalski <piotr.domagalski@yahoo.com>
2023-03-19 13:43:37 +0100

    dodawanie filmow

--- dom-06-demo-project-setup/assets/scripts/app.js
+++ dom-06-demo-project-setup/assets/scripts/app.js
@@ -4,6 +4,32 @@ const addNewMovieModal = document.getElementById("add-modal");
 const cancelAddMovieBatton = addNewMovieModal.querySelector(".btn--passive");
 const confirmAddMovieButton = cancelAddMovieBatton.nextElementSibling;
 const inputElements = addNewMovieModal.querySelectorAll("input");
+const entryTextSection = document.getElementById("entry-text");
+
+const movies = [];
+
+const updateUI = () => {
+  if (movies.length === 0) {
+    entryTextSection.style.display = "block";
+  } else {
+    entryTextSection.style.display = "none";
+  }
+};
+
+const renderNewMovieElement = (title, imageUrl, rating) => {
+  const newMovieElement = document.createElement("li");
+  newMovieElement.className = "movie-element";
+    newMovieElement.innerHTML = `
+    <div class="movie-element__image">
+        <img src="${imageUrl} alt="${title}">
+    </div>
+    <div class="movie-element__info">
+        <h2>${title}</h2>
+        <p>${rating}/5 stars</p>
+    </div>`;
+    const listRoot = document.getElementById('movie-list');
+    listRoot.appendChild(newMovieElement);
+};
 
 const toggleBackdrop = () => {
   backdrop.classList.toggle("visible");
@@ -11,6 +37,10 @@ const toggleBackdrop = () => {
 
 const toggleAddNewMovieModal = () => {
   addNewMovieModal.classList.toggle("visible");
+};
+
+const clearMovieInput = () => {
+  inputElements.forEach((input) => (input.value = ""));
 };
 
 function addNewMovieBtnClickHandler() {
@@ -37,7 +67,18 @@ function confirmAddMovieButtonHandler() {
     +ratingValue < 1 ||
     +ratingValue > 5
   ) {
-    alert('Please enter a valid input');
+    alert("Please enter a valid input");
+  } else {
+    const newMovie = {
+      title: titleValue,
+      image: imageUrlValue,
+      rating: +ratingValue,
+    };
+    movies.push(newMovie);
+    addNewMovieBtnClickHandler();
+    clearMovieInput();
+    renderNewMovieElement(titleValue, imageUrlValue, ratingValue);
+    updateUI();
   }
 }
 
