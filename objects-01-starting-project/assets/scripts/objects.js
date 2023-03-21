b8ace3ee5b595fa4879e2bad5bd67a7a039e0ed6
Piotrek Domagalski <piotr.domagalski@yahoo.com>
2023-03-21 09:59:26 +0100

    obiekty: miniprojekt start

--- objects-01-starting-project/assets/scripts/objects.js
+++ objects-01-starting-project/assets/scripts/objects.js
@@ -1,9 +1,32 @@
-const person = {
-  name: "Max",
-  age: 30,
-  hobbies: ["sport", "cooking"],
-  greet: () => alert("hi there"),
-};
+const addMovieBtn = document.getElementById("add-movie-btn");
+const searchBtn = document.getElementById("search-btn");
 
-person.isAdmin = true;
-console.log(person.isAdmin);
+const movies = [];
+
+function addMovieHandler() {
+  const inputsElements = document.querySelectorAll("#user-input input");
+  const [title, extraName, extraValue] = Array.from(inputsElements).map(
+    (ele) => ele.value
+  );
+
+  if (
+    title.trim() === "" ||
+    extraName.trim() === "" ||
+    extraValue.trim() === ""
+  ) {
+    return;
+  }
+
+  const newMovie = {
+    info: {
+      title: title,
+      [extraName]: extraValue,
+    },
+    id: Math.random(),
+  };
+
+  movies.push(newMovie);
+  console.log(movies);
+}
+
+addMovieBtn.addEventListener("click", addMovieHandler);
